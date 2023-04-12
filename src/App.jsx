5989ce6f1ffda7d3cc66a2b0ad928fd8b8178fed
Ataynadhif <ataynadhif29@gmail.com>
2023-04-12 19:07:21 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,19 @@ function App() {
       completed: false,
     },
   ]);
-
+  const toggleBtn = (todoId) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updateTodos)
+  }
   return (
       <div style={styles.container}>
         <h1 style={styles.title}>My Todo List</h1>
-        <Todos todos={todos}/>
+        <Todos todos={todos} toggleBtn={toggleBtn}/>
       </div>
   );
 }
